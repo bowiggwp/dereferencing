ddd8bb1876c8339422dbf464af94ad1ae55acd59
bowiggwp <118131019+bowiggwp@users.noreply.github.com>
2023-06-22 14:45:03 +0300

    Add files via upload

---
+++ Разадресация 3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int* a = new int(50);
+	cout << "adress \t *a\t b\n";
+	cout << a << '\t' << *a << endl;
+	(*a)++;
+	cout << a << '\t' << *a << endl;
+	(*a++);
+	cout << a << '\t' << *a << endl;
+	*a++;
+	cout << a << '\t' << *a << endl;
+	return 0;
+}
